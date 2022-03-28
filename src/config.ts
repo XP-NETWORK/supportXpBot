c9fe2cbcab78d0ac87497694af297015f87bf1bd
AlexeyAdoniev <Alexeya@xp.network>
2022-03-28 11:44:06 +0300

    config change

--- src/config.ts
+++ src/config.ts
@@ -11,13 +11,7 @@ function getOrThrow(key: string): string {
 }
 
 export default  {
-    contractAddress: getOrThrow('CONTRACT'),
-    redirect_url: getOrThrow('REDIRECT_URL'),
-    rpc: getOrThrow('RPC'),
     port: getOrThrow('PORT'),
-    fileName: getOrThrow('FILE_NAME'),
-    messageToSign: getOrThrow('MESSAGE_TO_SIGN'),
-    tokenList: Array.from(Array(Number(getOrThrow('TOKENS_NUMBER'))).keys()),
     bot: getOrThrow('BOT_TOKEN'),
     mongo: getOrThrow('MONGO_URI')
   
